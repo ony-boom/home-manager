2937db8c3e0959131683b290fe1aa6bf5ae88e52
Ony <onyrakoto27@gmail.com>
2024-07-21 14:14:35 +0300

    add foot terminal

--- home/programs/terminal/emulator/default.nix
+++ home/programs/terminal/emulator/default.nix
@@ -1,4 +1,7 @@
 {
+	imports = [
+		./foot.nix
+	];
   # Since nix has a issue with openGL and stuf, i'll install
   # alacritty using yay
   xdg.configFile."alacritty".source = ./alacritty;
---
+++ home/programs/terminal/emulator/foot.nix
@@ -0,0 +1,13 @@
+{
+	programs.foot = {
+		enable = true;
+		settings = {
+  main = {
+    term = "xterm-256color";
+    dpi-aware = "yes";
+  };
+
+};
+
+	};
+}
