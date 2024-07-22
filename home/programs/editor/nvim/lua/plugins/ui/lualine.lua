78a1d8ee243cb4caa5d8ec47b6a40589c686ea68
Ony <onyrakoto27@gmail.com>
2024-07-22 18:17:00 +0300

    add bufferline

--- home/programs/editor/nvim/lua/plugins/ui/lualine.lua
+++ home/programs/editor/nvim/lua/plugins/ui/lualine.lua
@@ -2,10 +2,29 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
 	config = function()
-		require("lualine").setup({
+		require('lualine').setup {
 			options = {
+				theme = 'auto',
+				component_separators = {
+					left = '|',
+					right = '|',
+				},
+				section_separators = {
+					left = '',
+					right = '',
+				},
+			},
 
-			}
-		})
+		 	sections = {
+    		lualine_a = {'mode'},
+    		lualine_b = {'branch'},
+    		lualine_c = {},
+    		lualine_x = {'encoding', 'fileformat', 'filetype'},
+    		lualine_y = {'progress'},
+    		lualine_z = {'location'}
+  		},
+
+			extensions = {'neo-tree'},
+		}
 	end
 }
