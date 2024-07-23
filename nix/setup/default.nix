46365f1ac4a2864bc88c2bfd6542ffd21c064c43
Ony <orakotonirina@bocasay.com>
2024-07-23 15:33:51 +0300

    add comment

--- nix/setup/default.nix
+++ nix/setup/default.nix
@@ -32,6 +32,7 @@ let
     runtimeInputs = with pkgs; [ git gum ];
     text = builtins.readFile ./scripts/${distro}.sh;
     runtimeEnv = scriptArgs { inherit extraPackages; };
+    # Just in case i choose to build the script instead of running it
     derivationArgs = scriptArgs { inherit extraPackages; };
   };
 
