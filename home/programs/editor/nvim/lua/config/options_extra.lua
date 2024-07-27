40f3eb03d1f102d0c2b60eaad059674ab6bb4a9c
Ony <onyrakoto27@gmail.com>
2024-07-27 16:31:30 +0300

    add lazygit, hehe!

--- home/programs/editor/nvim/lua/config/options_extra.lua
+++ home/programs/editor/nvim/lua/config/options_extra.lua
@@ -1,6 +1,8 @@
-vim.cmd([[
-  colorscheme catppuccin
-]])
+local themes = {
+	catppuccin = "catppuccin",
+	everforest = "everforest",
+}
+vim.cmd.colorscheme(themes.everforest)
 
 vim.o.showtabline = 2
 vim.cmd([[au FileType * if index(['wipe', 'delete'], &bufhidden) >= 0 | set nobuflisted | endif]])
