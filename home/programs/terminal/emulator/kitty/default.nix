8a76a37d53294ec139d5143698610cd5b1e502f8
Ony <onyrakoto27@gmail.com>
2024-09-08 11:26:22 +0300

    better kitty tab

--- home/programs/terminal/emulator/kitty/default.nix
+++ home/programs/terminal/emulator/kitty/default.nix
@@ -6,18 +6,18 @@
   ...
 }: let
   nixGL = import (self + /nix/nixGL.nix) {inherit pkgs config lib;};
-	themes = {
-		everforest = {
-			dark = {
-				medium = "Everforest Dark Medium";
-			};
-		};
-		rose-pine = {
-			dark = {
-				default = "Rosé Pine";
-			};
-		};
-	};
+  themes = {
+    everforest = {
+      dark = {
+        medium = "Everforest Dark Medium";
+      };
+    };
+    rose-pine = {
+      dark = {
+        default = "Rosé Pine";
+      };
+    };
+  };
 in {
   programs.kitty = {
     enable = true;
@@ -25,4 +25,6 @@ in {
     theme = themes.everforest.dark.medium;
     extraConfig = builtins.readFile ./kitty.conf;
   };
+
+  xdg.configFile."kitty/tab_bar.py".source = ./tab_bar.py;
 }
