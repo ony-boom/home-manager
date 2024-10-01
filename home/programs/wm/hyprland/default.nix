0fa1773b475e7a8756bcadf4e4169632fddf679f
Ony <onyrakoto27@gmail.com>
2024-10-01 08:10:17 +0300

    hyprland autostart

--- home/programs/wm/hyprland/default.nix
+++ home/programs/wm/hyprland/default.nix
@@ -1,9 +1,13 @@
 {
   lib,
   config,
+  # pkgs,
   ...
 }: {
   config = lib.mkIf config.programs.hyprland.enable {
     xdg.configFile."hypr".source = ./hypr;
+
+    # home.packages = with pkgs; [
+    # ];
   };
 }
