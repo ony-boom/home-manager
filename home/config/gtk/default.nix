b954bbd7c2318724df57f9b73e1dcb5335fe5dac
Ony <onyrakoto27@gmail.com>
2024-10-06 21:37:12 +0300

    make gtk theme more reproducible

--- home/config/gtk/default.nix
+++ home/config/gtk/default.nix
@@ -1,3 +1,13 @@
-{
+{pkgs, ...}: {
   xdg.configFile."gtk-3.0/gtk.css".source = ./gtk.css;
+
+  gtk = {
+    # INFO: Do not forget to install it, nix does not have it and I am lazy to package it
+    theme.name = "Rosepine-Dark";
+    cursorTheme = {
+      size = 24;
+      package = pkgs.rose-pine-cursor;
+      name = "BreezeX-RosePine-Linux";
+    };
+  };
 }
