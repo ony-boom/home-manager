a6ee531ccdf44ba56d1c90d2cd6568a83d725cf1
Ony <onyrakoto27@gmail.com>
2024-10-14 15:22:55 +0300

    remove hyprland warning

--- home/host/bocasay.nix
+++ home/host/bocasay.nix
@@ -1,7 +1,12 @@
 {
   gpuType = "intel";
 
-  wayland.windowManager.hyprland.enable = true;
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      monitors = [];
+    };
+  };
   programs.kitty.settings = {
     linux_display_server = "x11";
   };
