8d9712d2760da19ff9a1fe16fc142f8b62e781d8
Ony <onyrakoto27@gmail.com>
2024-10-17 09:34:46 +0300

    enable wayland on work laptop

--- home/host/bocasay.nix
+++ home/host/bocasay.nix
@@ -1,6 +1,10 @@
-{
+{stablePkgs, ...}: {
   gpuType = "intel";
 
+  wayland.windowManager.hyprland = {
+    enable = true;
+    package = stablePkgs.hyprland;
+  };
   programs.kitty.settings = {
     linux_display_server = "x11";
   };
