2b14402f1673b1b63ad04285648a631946da3740
Ony <onyrakoto27@gmail.com>
2024-11-03 11:56:06 +0300

    nvim default editor

--- home/programs/editor/default.nix
+++ home/programs/editor/default.nix
@@ -1,6 +1,9 @@
 {
   imports = [./vscode];
 
-  programs.neovim.enable = true;
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
   home.file.".ideavimrc".source = ./ideavimrc;
 }
