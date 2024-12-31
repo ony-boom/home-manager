5bb9824c307c69abbad7e833445301e12170b87e
Ony <onyrakoto27@gmail.com>
2024-12-31 21:09:28 +0300

    increase ghostty pading

--- home/programs/terminal/emulator/ghostty/default.nix
+++ home/programs/terminal/emulator/ghostty/default.nix
@@ -11,7 +11,27 @@
   };
 
   config = lib.mkIf config.programs.ghostty.enable {
-    xdg.configFile."ghostty".source = ./config;
+    xdg.configFile."ghostty/config".text = ''
+      font-size = 14
+      auto-update = off
+      theme = rose-pine
+
+      window-padding-y = 10
+      window-padding-x = 12
+      window-decoration = false
+      gtk-single-instance = true
+
+      background-opacity = 0.8
+      font-family = ${builtins.head config.fonts.fontconfig.defaultFonts.monospace}
+
+      # keybind = ctrl+shift+right=next_tab
+      # keybind = ctrl+shift+right=previous_tab
+      #
+      # keybind = ctrl+up=goto_split:top
+      # keybind = ctrl+left=goto_split:left
+      # keybind = ctrl+down=goto_split:bottom
+      # keybind = ctrl+right=goto_split:right
+    '';
 
     home.packages = with pkgs; [
       (pkgs.nixGL ghostty)
