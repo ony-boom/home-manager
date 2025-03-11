1944d412b7103fb5ed90cbda3688732fc563757d
Ony <onyrakoto27@gmail.com>
2025-03-11 11:44:02 +0300

    update ssh config for github multi account

--- home/programs/terminal/ssh.nix
+++ home/programs/terminal/ssh.nix
@@ -2,5 +2,22 @@
   programs.ssh = {
     enable = true;
     addKeysToAgent = "yes";
+    matchBlocks = {
+      gh = {
+        hostname = "github.com";
+        user = "git";
+        addKeysToAgent = "yes";
+        useKeychain = "yes";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+
+      gh-work = {
+        hostname = "github.com";
+        user = "git";
+        addKeysToAgent = "yes";
+        useKeychain = "yes";
+        identityFile = "~/.ssh/bocasay_gh";
+      };
+    };
   };
 }
