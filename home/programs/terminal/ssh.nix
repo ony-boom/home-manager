24041442ded4b81ac9f88be88fdf8733ca930877
Ony <onyrakoto27@gmail.com>
2025-03-11 19:49:16 +0300

    fix ssh conf

--- home/programs/terminal/ssh.nix
+++ home/programs/terminal/ssh.nix
@@ -6,16 +6,12 @@
       gh = {
         hostname = "github.com";
         user = "git";
-        addKeysToAgent = "yes";
-        useKeychain = "yes";
         identityFile = "~/.ssh/id_ed25519";
       };
 
       gh-work = {
         hostname = "github.com";
         user = "git";
-        addKeysToAgent = "yes";
-        useKeychain = "yes";
         identityFile = "~/.ssh/bocasay_gh";
       };
     };
