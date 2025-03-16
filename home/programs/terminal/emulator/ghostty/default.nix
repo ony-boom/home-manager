69dd459d86c3f4b9e2af2e8ade970218b92d3327
Ony <onyrakoto27@gmail.com>
2025-03-16 10:31:44 +0300

    update mms

--- home/programs/terminal/emulator/ghostty/default.nix
+++ home/programs/terminal/emulator/ghostty/default.nix
@@ -12,9 +12,7 @@
 
       window-padding-y = 10;
       window-padding-x = 12;
-      window-decoration = false;
       gtk-single-instance = true;
-      gtk-titlebar = false;
 
       background-opacity = 0.8;
       font-family = config.fonts.fontconfig.defaultFonts.monospace;
