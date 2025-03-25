700388eb612178050c8613f0527ce75e21ab7146
Ony <onyrakoto27@gmail.com>
2025-03-26 00:08:09 +0300

    update zed conf

--- home/programs/editor/zed.nix
+++ home/programs/editor/zed.nix
@@ -23,6 +23,9 @@ in
 
       userSettings =
         {
+          assistant = {
+            enabled = true;
+          };
           vim_mode = true;
           ui_font_size = 16;
           buffer_font_size = 16;
