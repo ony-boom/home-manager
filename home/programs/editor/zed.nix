98c9ef0ef62375d6a3562f845d23a98ceda96346
Ony <onyrakoto27@gmail.com>
2025-03-26 19:43:42 +0300

    manage zed config out of nix store

--- home/programs/editor/zed.nix
+++ home/programs/editor/zed.nix
@@ -1,77 +1,10 @@
-let
-  languages-settings = {
-    languages = {
-      Nix = {
-        language_servers = ["nil" "nixd"];
-        formatter.external = {
-          command = "alejandra";
-          args = ["--quiet" "--"];
-        };
-      };
-    };
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  programs.zed-editor = {
+    enable = lib.mkDefault true;
+    package = pkgs.nixGL pkgs.zed-editor;
   };
-in
-  {pkgs, lib, ...}: {
-    programs.zed-editor = {
-      enable = lib.mkDefault true;
-      package = pkgs.nixGL pkgs.zed-editor;
-      extensions = [
-        "rose-pine-theme"
-        "colored-zed-icons-theme"
-        "neosolarized"
-      ];
-
-      userSettings =
-        {
-          assistant = {
-            enabled = true;
-          };
-          vim_mode = true;
-          ui_font_size = 16;
-          buffer_font_size = 16;
-
-          vim = {
-            use_system_clipboard = "always";
-            toggle_relative_line_numbers = true;
-          };
-
-          features = {
-            edit_prediction_provider = "zed";
-          };
-
-          theme = {
-            mode = "system";
-            dark = "NeoSolarized Dark";
-            light = "NeoSolarized Light";
-          };
-
-          icon_theme = {
-            dark = "Colored Zed Icons Theme Dark";
-            light = "Colored Zed Icons Theme Light";
-          };
-        }
-        // languages-settings;
-
-      userKeymaps = [
-        {
-          context = "Editor && vim_mode == normal";
-          bindings = {
-            "space l f" = "editor::Format";
-            "space l r" = "editor::Rename";
-            "space c" = "pane::CloseActiveItem";
-            "] b" = "pane::ActivateNextItem";
-            "[ b" = "pane::ActivatePrevItem";
-            "space e" = "workspace::ToggleLeftDock";
-            "space f f" = "file_finder::Toggle";
-            "space f w" = "pane::DeploySearch";
-          };
-        }
-        {
-          context = "ProjectPanel";
-          bindings = {
-            "space e" = "workspace::ToggleLeftDock";
-          };
-        }
-      ];
-    };
-  }
+}
