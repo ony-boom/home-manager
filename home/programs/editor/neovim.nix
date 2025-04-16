7359f4d8256ef48398e78187bd6ed0d2286f28a9
Ony <onyrakoto27@gmail.com>
2025-04-16 16:28:08 +0300

    update neovin conf

--- home/programs/editor/neovim.nix
+++ home/programs/editor/neovim.nix
@@ -1,18 +1,29 @@
-{ config, pkgs,... }:
 {
+  config,
+  pkgs,
+  ...
+}: {
   programs.neovim = {
     enable = true;
     defaultEditor = true;
   };
 
-   xdg.configFile."nvim" = {
-     source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Repos/mine/neovim-config";
-     recursive = true;
-   };
+  xdg.configFile."nvim" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Repos/mine/neovim-config";
+    recursive = true;
+  };
 
-   home.packages = with pkgs; [
-      alejandra
-      stylua
-      prettierd
-   ];
+  home.packages = with pkgs; [
+    # format
+    alejandra
+    stylua
+    prettierd
+
+    # lsp
+
+    nil
+    lua-language-server
+    vscode-langservers-extracted
+    typescript-language-server
+  ];
 }
