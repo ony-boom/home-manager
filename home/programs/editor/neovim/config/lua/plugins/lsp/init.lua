038f0af4f3a120086b35b92ac7c83a3a5bdca57e
Ony <onyrakoto27@gmail.com>
2025-05-05 23:10:46 +0300

    fix deno/tsls lsp

--- home/programs/editor/neovim/config/lua/plugins/lsp/init.lua
+++ home/programs/editor/neovim/config/lua/plugins/lsp/init.lua
@@ -1,8 +1,8 @@
 return {
   "neovim/nvim-lspconfig",
   dependencies = { "saghen/blink.cmp", "b0o/schemastore.nvim" },
-
   config = function()
+    local lspconfig = require "lspconfig"
     local snippets_capabilities = {
       textDocument = {
         completion = {
@@ -25,12 +25,9 @@ return {
       },
       astro = {},
       rust_analyzer = {},
-      eslint = {},
       svelte = {},
       tailwindcss = {},
       emmet_language_server = {},
-      vtsls = {},
-      denols = {},
       jsonls = {
         capabilities = snippets_capabilities,
         settings = {
@@ -42,9 +39,26 @@ return {
       },
     }
 
+    local servers_with_setup = {
+      eslint = {},
+      denols = {
+        root_dir = lspconfig.util.root_pattern { "deno.json", "deno.jsonc" },
+      },
+      vtsls = {
+        root_dir = lspconfig.util.root_pattern "package.json",
+        single_file_support = false,
+      },
+    }
+
+    -- Standard server setup
     for name, config in pairs(servers) do
       if next(config) ~= nil then vim.lsp.config(name, config) end
       vim.lsp.enable(name)
+    end
+
+    -- Servers with custom setup
+    for name, config in pairs(servers_with_setup) do
+      lspconfig[name].setup(config)
     end
 
     require "plugins.lsp.lsp_attach"
