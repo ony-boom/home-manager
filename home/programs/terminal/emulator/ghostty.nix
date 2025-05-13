e65b116f0d15c1f2fd4ed457d89ab0d5663fc9d2
Ony <onyrakoto27@gmail.com>
2025-05-13 18:39:08 +0300

    add some blur

--- home/programs/terminal/emulator/ghostty.nix
+++ home/programs/terminal/emulator/ghostty.nix
@@ -18,7 +18,7 @@
       gtk-single-instance = true;
 
       font-size = 14;
-      # background-opacity = 0.8;
+      background-opacity = 0.9;
       shell-integration = "zsh";
       shell-integration-features = "sudo";
       font-family = builtins.head config.fonts.fontconfig.defaultFonts.monospace;
