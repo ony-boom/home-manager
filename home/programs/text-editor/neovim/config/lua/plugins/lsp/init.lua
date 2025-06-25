9d6e06297831aa741f69fd98738f627629e9cc16
Ony <onyrakoto27@gmail.com>
2025-06-25 17:51:12 +0300

    refactor

--- home/programs/text-editor/neovim/config/lua/plugins/lsp/init.lua
+++ home/programs/text-editor/neovim/config/lua/plugins/lsp/init.lua
@@ -3,53 +3,10 @@ return {
   dependencies = { "saghen/blink.cmp", "b0o/schemastore.nvim" },
   config = function()
     local lspconfig = require "lspconfig"
-    local snippets_capabilities = {
-      textDocument = {
-        completion = {
-          completionItem = {
-            snippetSupport = true,
-          },
-        },
-      },
-    }
-
-    local servers = {
-      lua_ls = {},
-      nil_ls = {},
-      gopls = {},
-      nushell = {},
-      html = {
-        capabilities = snippets_capabilities,
-      },
-      cssls = {
-        capabilities = snippets_capabilities,
-      },
-      astro = {},
-      rust_analyzer = {},
-      svelte = {},
-      emmet_language_server = {},
-      jsonls = {
-        capabilities = snippets_capabilities,
-        settings = {
-          json = {
-            schemas = require("schemastore").json.schemas(),
-            validate = { enable = true },
-          },
-        },
-      },
-      eslint = {},
-      tailwindcss = {},
-      denols = {
-        root_dir = lspconfig.util.root_pattern { "deno.json", "deno.jsonc" },
-      },
-      ts_ls = {
-        root_dir = lspconfig.util.root_pattern "package.json",
-        single_file_support = false,
-      },
-    }
+    local getServers = require "plugins.lsp.servers"
 
     -- Servers with custom setup
-    for name, config in pairs(servers) do
+    for name, config in pairs(getServers()) do
       lspconfig[name].setup(config)
     end
 
---
+++ home/programs/text-editor/neovim/config/lua/plugins/lsp/servers.lua
@@ -0,0 +1,47 @@
+return function()
+  local lspconfig = require "lspconfig"
+  local snippets_capabilities = {
+    textDocument = {
+      completion = {
+        completionItem = {
+          snippetSupport = true,
+        },
+      },
+    },
+  }
+
+  return {
+    lua_ls = {},
+    nil_ls = {},
+    gopls = {},
+    nushell = {},
+    html = {
+      capabilities = snippets_capabilities,
+    },
+    cssls = {
+      capabilities = snippets_capabilities,
+    },
+    astro = {},
+    rust_analyzer = {},
+    svelte = {},
+    emmet_language_server = {},
+    jsonls = {
+      capabilities = snippets_capabilities,
+      settings = {
+        json = {
+          schemas = require("schemastore").json.schemas(),
+          validate = { enable = true },
+        },
+      },
+    },
+    eslint = {},
+    tailwindcss = {},
+    denols = {
+      root_dir = lspconfig.util.root_pattern { "deno.json", "deno.jsonc" },
+    },
+    ts_ls = {
+      root_dir = lspconfig.util.root_pattern "package.json",
+      single_file_support = false,
+    },
+  }
+end
