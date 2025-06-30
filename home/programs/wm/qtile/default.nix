a975f0569f534a0ac0763b81ce6ff77a12d4e9b0
Ony <onyrakoto27@gmail.com>
2025-06-30 19:22:22 +0300

    manage qtile

---
+++ home/programs/wm/qtile/default.nix
@@ -0,0 +1,9 @@
+# Install qtile through system's package manager, installing it through nix is quite problematic or it's just a skill issue xD.
+{utils, ...}: {
+  xdg.configFile = {
+    "qtile" = {
+      source = utils.mkDots "/wm/qtile/config";
+      recursive = true;
+    };
+  };
+}
