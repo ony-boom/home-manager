8794b23734051895d6d6c3615b85a36a1c5494a8
Ony <onyrakoto27@gmail.com>
2025-07-02 14:33:57 +0300

    add polybar bar to qtile

--- home/programs/wm/polybar/config/launch.sh
+++ home/programs/wm/polybar/config/launch.sh
@@ -6,7 +6,7 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch
+# Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybarmain.log
 polybar main 2>&1 | tee -a /tmp/polybarmain.log &
 disown
