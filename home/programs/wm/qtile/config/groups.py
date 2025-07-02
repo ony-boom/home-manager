b9f08ca2457fb313981213855921c3138e3b2c1d
Ony <onyrakoto27@gmail.com>
2025-07-02 07:09:07 +0300

    split qtile config

---
+++ home/programs/wm/qtile/config/groups.py
@@ -0,0 +1,30 @@
+from libqtile.config import Group, Key
+from libqtile.lazy import lazy
+
+
+def init_group(mod, keys):
+    groups = [Group(i) for i in "123456789"]
+
+    for i in groups:
+        keys.extend(
+            [
+                # mod + group number = switch to group
+                Key(
+                    [mod],
+                    i.name,
+                    lazy.group[i.name].toscreen(),
+                    desc=f"Switch to group {i.name}",
+                ),
+                # mod + shift + group number = switch to & move focused window to group
+                Key(
+                    [mod, "shift"],
+                    i.name,
+                    lazy.window.togroup(i.name, switch_group=True),
+                    desc=f"Switch to & move focused window to group {i.name}",
+                ),
+                # Or, use below if you prefer not to switch to that group.
+                # # mod + shift + group number = move focused window to group
+                # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+                #     desc="move focused window to group {}".format(i.name)),
+            ]
+        )
