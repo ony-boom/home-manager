f5ec369617601dd026a7418d5bb9b311c63b6579
Ony <onyrakoto27@gmail.com>
2025-07-04 09:49:07 +0300

    revert

--- host/BCS-MG-LP-0120-09/default.nix
+++ host/BCS-MG-LP-0120-09/default.nix
@@ -1,10 +1,8 @@
-{pkgs, ...}: {
+{
   nixGL = {
     defaultWrapper = "mesa";
     installScripts = ["mesa"];
   };
-
-  programs.rofi.package = pkgs.rofi-wayland;
 
   services = {
     mpd.enable = false;
