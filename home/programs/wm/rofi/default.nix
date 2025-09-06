7d2a044a3a1988929a5c190b0eb65fb94e4917fa
Ony <onyrakoto27@gmail.com>
2025-09-06 08:03:27 +0300

    configured cliphist

--- home/programs/wm/rofi/default.nix
+++ home/programs/wm/rofi/default.nix
@@ -1,6 +1,34 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  utils,
+  ...
+}: {
   programs.rofi = {
     enable = true;
     package = pkgs.rofi-wayland;
+    theme = "rose-pine";
+
+    font = "monospace 12";
+
+    extraConfig = {
+      show-icons = true;
+      display-drun = "";
+      display-clipboard = "";
+    };
+
+    plugins = with pkgs; [
+      rofi-calc
+    ];
+  };
+
+  xdg.configFile."rofi/themes" = {
+    source = utils.mkDots "/wm/rofi/themes";
+    recursive = true;
+  };
+
+
+  xdg.configFile."rofi/scripts" = {
+    source = utils.mkDots "/wm/rofi/scripts";
+    recursive = true;
   };
 }
